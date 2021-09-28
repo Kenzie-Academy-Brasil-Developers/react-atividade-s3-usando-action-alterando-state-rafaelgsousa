675e4b87d79dbffc9e53b08bc9f20654998df6fb
rafaeldev <elderrafaelgomes@gmail.com>
2021-09-28 10:55:29 -0300

    fix & style: soluçao de um bug e estilizaçao do card

--- usando-action/src/Components/UseCard/index.jsx
+++ usando-action/src/Components/UseCard/index.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import changeName from "./../../store/modules/user/actions";
+import "./style.css"
 
 const UserCard = () => {
 
@@ -16,8 +17,8 @@ const UserCard = () => {
     }
 
     return (
-        <div>
-            <p>Nome: {nameuser}</p>
+        <div className="card">
+            <p>User name:<span>{nameuser.name}</span></p>
             <input type="text" onChange={(e)=>setName(e.target.value)}/>
             <button onClick={handleClick}>Change</button>
         </div>
